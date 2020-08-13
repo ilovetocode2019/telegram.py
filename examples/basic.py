1e0d742bb3d59acf91dd6b69bfaef654a61697fb
Fyssion <fyssioncodes@gmail.com>
2020-08-13 16:01:34 -0500

    Add comments to examples and fix typos

--- examples/basic.py
+++ examples/basic.py
@@ -4,10 +4,14 @@ import logging
 logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
 logger = logging.getLogger("pygram")
 
+# Make sure never to share your token
 bot = pygram.Bot("token here")
 
-@bot.command(name="hi")
-async def send(ctx):
+# Create and register a simple command called "hi".
+# This is invoked with "/hi"
+# and will make the bot reply with "Hello"
+@bot.command()
+async def hi(ctx):
     await ctx.send("Hello")
-    
+
 bot.run()
--- examples/poll.py
+++ examples/poll.py
@@ -4,18 +4,18 @@ import logging
 logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
 logger = logging.getLogger("pygram")
 
+# Make sure never to share your token
 bot = pygram.Bot("token here")
 
-@bot.command(name="poll")
-async def send(ctx):
+@bot.command()
+async def poll(ctx):
     await ctx.send_poll(question="What is your favorite pet?", options=["Dogs", "Cats"])
 
 @bot.event
 async def on_poll(poll):
-    #This runs when a poll updates or a poll is created
-    #You can use this to collect poll answers
-    #A poll class is automaticly passed in
-
+    # This runs when a poll updates or a poll is created.
+    # You can use this to collect poll answers.
+    # A poll class is automatically passed in.
     pass
     
 bot.run()
