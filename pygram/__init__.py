63d5307b33270fb6f9fea797677e3750bd7ce124
Fyssion <fyssioncodes@gmail.com>
2020-08-13 16:10:42 -0500

    Add license to all lib files and add info to __init__.py

--- pygram/__init__.py
+++ pygram/__init__.py
@@ -1,3 +1,20 @@
+"""
+telegram.py: Telegram Bot API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A simple API wrapper for the Telegram Bot API.
+
+:copyright: (c) 2020 ilovetocode
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "pygram"
+__author__ = "ilovetocode"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 ilovetocode"
+__version__ = "0.1.0a"
+
+from collections import namedtuple
+
 from .bot import Bot
 from .errors import *
 from .chat import Chat
@@ -9,4 +26,6 @@ from .poll import Poll
 from .core import *
 from .cog import Cog
 
-__version__ = "0.1.0a"
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
