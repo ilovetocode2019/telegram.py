75a842db3ebc95273417c35600ff785b6181bf04
Fyssion <fyssioncodes@gmail.com>
2020-08-15 13:59:37 -0500

    Revert version line back
    I accidentally formatted with an autoformatter (black formatter), which did this.

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ with open("requirements.txt") as f:
 
 version = ""
 with open("pygram/__init__.py") as f:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
-    ).group(1)
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 setuptools.setup(
     name="telegram.py",
