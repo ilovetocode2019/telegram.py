78e57ae711ed5f392e18e52c1300bd5c31e27cf4
ilovetocode <nathanwj123@outlook.com>
2020-08-17 17:27:48 -0700

    Update setuptools.py

--- setup.py
+++ setup.py
@@ -37,8 +37,10 @@ setuptools.setup(
         ]
     },
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
