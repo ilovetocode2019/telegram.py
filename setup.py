00312998d794e0868152b743bd595556778ae484
Fyssion <fyssioncodes@gmail.com>
2020-08-17 21:17:24 -0500

    Add 3.6, 3.7, and 3.8 to versions

--- setup.py
+++ setup.py
@@ -38,7 +38,9 @@ setuptools.setup(
     },
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
