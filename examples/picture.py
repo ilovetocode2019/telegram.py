c47d83d2fa40fb5da800b2aa43b3e037d7f92000
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2021-04-28 14:10:30 -0700

    Improve examples

--- examples/picture.py
+++ examples/picture.py
@@ -6,23 +6,17 @@ from telegrampy.ext import commands
 logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
 logger = logging.getLogger("telegrampy")
 
-# Make sure never to share your token
+
 bot = commands.Bot("token here")
 
-# Create and register a command called "image".
-# Note that you can specify the command name
-# in the command decorator.
+
 @bot.command(name="image")
 async def image_command(ctx):
-    # Make the bot start typing
-    await ctx.send_action("typing")
+    # Send the action 'upload_photo'
+    await ctx.send_action("upload_photo")
 
-    # Open an image...
-    with open("file path here", "rb") as f:
-        # ...convert it to a sendable photo...
-        photo = telegrampy.Photo(file=f, caption="This is a photo")
-
-    # ...and upload/send it.
-    await ctx.send(file=photo)
+    # Open an image and send it to the chat
+    with open("file path here", "rb") as photo:
+        await ctx.send_photo(photo, filename="photo.png", capton="This is a photo")
 
 bot.run()
--- examples/poll.py
+++ examples/poll.py
@@ -6,18 +6,11 @@ from telegrampy.ext import commands
 logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
 logger = logging.getLogger("telegrampy")
 
-# Make sure never to share your token
 bot = commands.Bot("token here")
+
 
 @bot.command()
 async def poll(ctx):
     await ctx.send_poll(question="What is your favorite pet?", options=["Dogs", "Cats"])
-
-@bot.event
-async def on_poll(poll):
-    # This runs when a poll updates or a poll is created.
-    # You can use this to collect poll answers.
-    # A poll class is automatically passed in.
-    pass
 
 bot.run()
